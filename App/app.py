3bc245b40d6fb9dd9e7dc32dae5f4d3984529400
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-19 00:40:39 +0100

    Data Loading and Model Configuration

--- App/app.py
+++ App/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 # Page configuration
 st.set_page_config(
@@ -15,4 +19,21 @@ st.write("Luna is an app that allows you to upload your datasets, choose machine
 st.header("Get Started!")
 
 # File Uploader Widget
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv", "xlsx"])
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    # Reading the dataset
+    df = pd.read_csv(uploaded_file)
+
+    # Sidebar options
+    st.sidebar.header("Model Configuration")
+    target_col = st.sidebar.selectbox("Select Target Column", df.columns)
+    algorithms = st.sidebar.multiselect("Select Algorithms", ["Random Forest", "Gradient Boosting", "SVM"])
+
+    # Splits the dataset
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    
+    
