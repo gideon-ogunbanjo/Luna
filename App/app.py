44cb46f18f7f07779ff4f1987826de0ad944ae13
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-19 00:34:21 +0100

    set page configuration

--- App/app.py
+++ App/app.py
@@ -2,3 +2,9 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+# Page configuration
+st.set_page_config(
+    page_title="Interactive Model Tuning App",
+    layout="centered"
+)
