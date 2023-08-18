9088fa933c12f48258c9a33d659976e78b9a7cb9
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-19 00:36:04 +0100

    created File upload widget

--- App/app.py
+++ App/app.py
@@ -11,3 +11,6 @@ st.set_page_config(
 
 # App title
 st.title("Luna - Interactive Model Tuning App")
+
+# File Uploader Widget
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv", "xlsx"])
