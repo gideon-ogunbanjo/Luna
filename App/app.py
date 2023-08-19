0f70b3a00ef033d2e81a5ec053715cec3a50b820
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-19 19:04:19 +0100

    edited app descriotion

--- App/app.py
+++ App/app.py
@@ -17,8 +17,8 @@ st.set_page_config(
 
 # App title
 st.title("Luna - Interactive Model Tuning App")
-st.write("Luna is an app that allows you to upload your datasets, choose machine learning algorithms, and interactively tune hyperparameters and see the effects on model performance.")
-st.header("Get Started!")
+st.write("Luna is an app thatnallows users to upload a CSV dataset, select different machine learning algorithms, tune hyperparameters, and view the model performance metrics")
+st.header("Get Started Now!")
 
 # Upload dataset
 uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
